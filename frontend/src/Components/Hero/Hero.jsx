655ae07086ecfdcf4c006df12768828273cd79a7
Dido2002 <didoguzelev@gmail.com>
2024-03-01 11:36:30 +0200

    HeroComponent

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Hero.css'
+import hand_icon from '../Assets/hand_icon.png'
+import arrow_icon from '../Assets/arrow.png'
+import hero_image from '../Assets/hero_image.png'
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="hero-left">
+            <h2>New Arrivals Only</h2>
+            <div>
+                <div className="hero-hand-icon">
+                    <p>New</p>
+                    <img src={hand_icon} alt="there is no image"/>
+                </div>
+                <p>Collection</p>
+                <p>For Everyone</p>
+            </div>
+            <div className="hero-latest-btn">
+                <div>
+                    Latest Collection
+                </div>
+                <img src={arrow_icon} alt='there is no image'/>
+            </div>
+        </div>
+        <div className="hero-right">
+            <img src={hero_image} alt="there is no image"/>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,62 @@
-import React, {useState} from 'react'
-import './Navbar.css'
-import logo from "../Assets/logo.png"
-import cart_icon from "../Assets/cart_icon.png"
+import React, { useState } from "react";
+import {Link} from 'react-router-dom';
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
-
   const [menu, setMenu] = useState("shop");
 
   return (
     <div className="navbar">
       <div className="nav-logo">
-        <img src={logo} alt="There is no image"/>
+        <img src={logo} alt="There is no image" />
         <p>Shopper</p>
       </div>
-      <ul className='nav-menu'>
-        <li onClick={() => {setMenu("shop")}}>Shop {menu === "shop" ? <hr/> : null}</li>
-        <li onClick={() => {setMenu("mens")}}>Mens {menu === "mens" ? <hr/> : null}</li>
-        <li onClick={() => {setMenu("womens")}}>Womens {menu === "womens" ? <hr/> : null}</li>
-        <li onClick={() => {setMenu("kids")}}>Kids {menu === "kids" ? <hr/> : null}</li>
+      <ul className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          <Link style={{textDecoration:"none"}} to="/">Shop</Link> {menu === "shop" ? <hr /> : null}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
+          <Link to="/mens" style={{textDecoration:"none"}}>Mens</Link>
+          {menu === "mens" ? <hr /> : null}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          <Link to="/womens" style={{textDecoration:"none"}}>Womens</Link>
+          {menu === "womens" ? <hr /> : null}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          <Link to="/kids" style={{textDecoration:"none"}}>Kids</Link>
+          {menu === "kids" ? <hr /> : null}
+        </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="There is no image"/>
-        <div className="nav-cart-count">
-          0
-        </div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="There is no image" />
+        </Link>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
