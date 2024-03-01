56e7d19f22d0d802780c33a923ada4379d9565eb
Dido2002 <didoguzelev@gmail.com>
2024-03-01 12:34:58 +0200

    itemComponent+PopularComponent

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className="hero">
         <div className="hero-left">
-            <h2>New Arrivals Only</h2>
+            <h2>New arrivals only</h2>
             <div>
                 <div className="hero-hand-icon">
                     <p>New</p>
