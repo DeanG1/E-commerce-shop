bd8a7be926b300f2f38bff39aa0ceacafa8305c8
Dido2002 <didoguzelev@gmail.com>
2024-03-03 09:23:00 +0200

    ShopCategoryComponent+ShopContext

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
-
-const ShopCategory = () => {
+import React, {useContext} from 'react'
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import Item from '../Components/Items/Item';
+const ShopCategory = (props) => {
+  const {all_products} = useContext(ShopContext)
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img className='shopcategory-banner' src={props.banner} alt="there is no image"/>
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Show 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt='there is no image'/>
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_products.map((item, i) => {
+            if(props.category === item.category){
+              return <Item key={i} id={item.id} 
+              name={item.name} image={item.image}
+              old_price={item.old_price}
+              new_price={item.new_price}/>
+            }
+            else{
+              return null;
+            }
+        })}
+      </div>
+      <div className='shopcategory-loadmore'>
+        Explore More
+      </div>
+    </div>
   )
 }
 
