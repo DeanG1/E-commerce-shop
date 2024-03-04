f005f8092cb65f46a09d47f4b2b3202fc1ccd8b0
Dido2002 <didoguzelev@gmail.com>
2024-03-04 10:24:21 +0200

    QuickFixes+RelatedProductsContent

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -3,6 +3,7 @@ import './ProductDisplay.css'
 import star_icon from '../Assets/star_icon.png'
 import star_dull_icon from '../Assets/star_dull_icon.png'
 
+
 const ProductDisplay = (props) => {
     const {product} = props;
   return (
--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -4,7 +4,7 @@ import { ShopContext } from '../Context/ShopContext';
 import dropdown_icon from "../Components/Assets/dropdown_icon.png";
 import Item from '../Components/Items/Item';
 const ShopCategory = (props) => {
-  const {all_products} = useContext(ShopContext)
+  const {all_product} = useContext(ShopContext)
   return (
     <div className='shop-category'>
       <img className='shopcategory-banner' src={props.banner} alt="there is no image"/>
@@ -17,7 +17,7 @@ const ShopCategory = (props) => {
         </div>
       </div>
       <div className="shopcategory-products">
-        {all_products.map((item, i) => {
+        {all_product.map((item, i) => {
             if(props.category === item.category){
               return <Item key={i} id={item.id} 
               name={item.name} image={item.image}
