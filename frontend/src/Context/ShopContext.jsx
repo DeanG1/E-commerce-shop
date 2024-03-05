edb8cd37b4dc960e2c77467e040b7ee26b14ba8a
Dido2002 <didoguzelev@gmail.com>
2024-03-05 09:16:44 +0200

    getTotalCartAmountLogic&Functionality

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -21,7 +21,27 @@ const ShopContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev)=> ({...prev, [itemId]: prev[itemId] - 1}));
     }
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart};
+
+
+    //This function will allow us to get the total amount of items in the cart
+    //First we will create a variable that will store the total amount
+    //Then we will loop through all the items in the cart
+    //If the item is in the cart we will add the price of the item to the total amount
+    //Finally we will return the total amount
+    //This will allow us to get the total amount of items in the cart
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                let itemInfo = all_product.find((product) => product.id === Number(item));
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+        return totalAmount;
+
+    }
+
+    const contextValue = {getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart, };
 
     return(
         <ShopContext.Provider value={contextValue}>
